836ab9b2a77dab733199e20792a4a97aa5c81080
Yohan-Baechle <baechle.yohan.gmail.com>
2023-12-26 10:04:42 +0100

    Création de la resource Post

--- backend/app/Http/Controllers/Api/PostController.php
+++ backend/app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -11,8 +12,7 @@ class PostController extends Controller
     public function index()
     {
         // Affiche tous les articles
-        $posts = Post::all();
-        return response()->json($posts);
+        return PostResource::collection(Post::limit(5)->get());
     }
 
     public function show($id)
--- backend/app/Http/Resources/PostResource.php
+++ backend/app/Http/Resources/PostResource.php
@@ -14,6 +14,11 @@ class PostResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'picture' => $this->picture,
+            'content' => $this->content
+        ];
     }
 }
